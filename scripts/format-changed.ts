aef872ccbe4f145fccdbfeba77c6d8ce7e24f4be
Justin Bennett <git@just-be.dev>
2024-02-01 22:15:42 -0500

    Update markdown, tweak pre-commit

--- scripts/format-changed.ts
+++ scripts/format-changed.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env deno run --allow-env --allow-read --allow-run
 import $ from "https://deno.land/x/dax@0.37.1/mod.ts";
 
-const filesChanged = await $`git diff --cached --name-only`.lines();
+const filesChanged = (await $`git diff --cached --name-only`.lines()).filter(
+  Boolean,
+);
 
-await $`deno fmt ${filesChanged}`;
-await $`git add ${filesChanged}`;
+if (filesChanged.length > 0) {
+  await $`deno fmt ${filesChanged}`;
+  await $`git add ${filesChanged}`;
+}
